fbb73262f7495f94c225971bf1c071a34b63468b
FelipeArredond <pipechiqui77@gmail.com>
2024-04-03 12:03:07 -0500

    Refactor registration form and update API endpoint

--- bafrontend/app/registro-acopio/page.tsx
+++ bafrontend/app/registro-acopio/page.tsx
@@ -2,14 +2,16 @@
 
 import { RefObject, useRef } from "react";
 import { CreateCollectionPoint } from "utils/collectionPoint/collectionPoint.service";
-import { CollectionPointCreateRequest } from "utils/collectionPoint/collectionPoint.service";
+import Image from "next/image";
+import logo from "../../public/images/baLogo.webp";
 
 export default function Page() {
   const userIdRef: RefObject<HTMLInputElement> = useRef(null);
-  const nameRef: RefObject<HTMLInputElement> = useRef(null);
+  const userNameRef: RefObject<HTMLInputElement> = useRef(null);
   const lastName: RefObject<HTMLInputElement> = useRef(null);
   const mailRef: RefObject<HTMLInputElement> = useRef(null);
   const numberRef: RefObject<HTMLInputElement> = useRef(null);
+  const nameRef: RefObject<HTMLInputElement> = useRef(null);
   const aggrementRef: RefObject<HTMLInputElement> = useRef(null);
   const addressRef: RefObject<HTMLInputElement> = useRef(null);
   const cityRef: RefObject<HTMLInputElement> = useRef(null);
@@ -20,10 +22,11 @@ export default function Page() {
     event?.preventDefault();
     const response = await CreateCollectionPoint({
       userId: userIdRef.current?.value ?? "",
-      name: nameRef.current?.value ?? "",
+      userName: userNameRef.current?.value ?? "",
       lastName: lastName.current?.value ?? "",
       email: mailRef.current?.value ?? "",
       phoneNumber: numberRef.current?.value ?? "",
+      name: nameRef.current?.value ?? "",
       agreement: aggrementRef.current?.value ?? "",
       address: addressRef.current?.value ?? "",
       city: cityRef.current?.value ?? "",
@@ -37,8 +40,11 @@ export default function Page() {
   return (
     <div>
       <div className="flex flex-col gap-3 justify-center items-center shadow-xl w-1/4 m-auto mt-[10%] p-5">
-        <h3 className="text-3xl font-bold text-var-red">
-          Formulario de solicitud para punto de acopio
+        <header className="bg-white">
+          <Image src={logo} alt="baLogo" />
+        </header>
+        <h3 className="text-3xl text-center font-bold text-var-red">
+          Formulario de registro de punto de acopio
         </h3>
         <div className="flex flex-col justify-center items-center gap-3">
           <div className="flex flex-col items-center">
@@ -48,27 +54,27 @@ export default function Page() {
               name="id"
               id="id"
               ref={userIdRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <div className="flex flex-col items-center">
-            <label htmlFor="name">Nombre</label>
+            <label htmlFor="name">Nombre del representante</label>
             <input
               type="text"
               name="name"
               id="name"
-              ref={nameRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              ref={userNameRef}
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <div className="flex flex-col items-center">
-            <label htmlFor="password">Apellido</label>
+            <label htmlFor="password">Apellido  del representante</label>
             <input
               type="text"
               name="password"
               id="password"
               ref={lastName}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
             <div className="flex flex-col items-center">
               <label htmlFor="mail">Correo</label>
@@ -77,7 +83,7 @@ export default function Page() {
                 name="mail"
                 id="mail"
                 ref={mailRef}
-                className="border border-solid border-var-blue rounded-sm p-1"
+                className="border border-solid border-var-blue rounded-lg p-1"
               />
             </div>
           </div>
@@ -88,7 +94,17 @@ export default function Page() {
               name="text"
               id="text"
               ref={numberRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
+            />
+          </div>
+          <div className="flex flex-col items-center">
+            <label htmlFor="text">Nombre</label>
+            <input
+              type="text"
+              name="text"
+              id="text"
+              ref={nameRef}
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <div className="flex flex-col items-center">
@@ -98,7 +114,7 @@ export default function Page() {
               name="text"
               id="text"
               ref={aggrementRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <div className="flex flex-col items-center">
@@ -108,7 +124,7 @@ export default function Page() {
               name="text"
               id="text"
               ref={addressRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <div className="flex flex-col items-center">
@@ -118,7 +134,7 @@ export default function Page() {
               name="text"
               id="text"
               ref={cityRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <div className="flex flex-col items-center">
@@ -128,7 +144,7 @@ export default function Page() {
               name="text"
               id="text"
               ref={stateRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <div className="flex flex-col items-center">
@@ -138,11 +154,11 @@ export default function Page() {
               name="text"
               id="text"
               ref={countryRef}
-              className="border border-solid border-var-blue rounded-sm p-1"
+              className="border border-solid border-var-blue rounded-lg p-1"
             />
           </div>
           <button
-            className="p-1 rounded-sm bg-var-red text-white"
+            className="p-1 rounded-lg bg-var-red text-white"
             onClick={() => handleLogin()}
           >
             Crear punto de acopio
